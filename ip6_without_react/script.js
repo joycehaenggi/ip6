1d7d7ec2f3d95569154afc0aebaddcad4073ef2f
Florian Daasch <florian.daasch@students.fhnw.ch>
2021-12-25 14:48:21 +0100

    Added tooltip

--- ip6_without_react/script.js
+++ ip6_without_react/script.js
@@ -4,3 +4,16 @@ const navbarLinks = document.getElementsByClassName('header-links')[0]
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
+
+function showTooltip(evt, text) {
+    let tooltip = document.getElementById("tooltip");
+    tooltip.innerHTML = text;
+    tooltip.style.display = "block";
+    tooltip.style.left = evt.pageX + 10 + 'px';
+    tooltip.style.top = evt.pageY + 10 + 'px';
+}
+
+function hideTooltip() {
+    var tooltip = document.getElementById("tooltip");
+    tooltip.style.display = "none";
+}
