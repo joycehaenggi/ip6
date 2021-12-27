7d1fa0b0ab7a9fb15701e3c67aa8ab4b8cdca8c7
Florian Daasch <florian.daasch@students.fhnw.ch>
2021-12-27 15:12:40 +0100

    Updated animation at progress bar

--- ip6_without_react/script.js
+++ ip6_without_react/script.js
@@ -7,53 +7,6 @@ toggleButton.addEventListener('click', () => {
 })
 
 
-
-/*// Function to increase image size
-function resizeNotification() {
-    if (x === 0) {
-
-        let id = null;
-        let pos = 100;
-        clearInterval(id);
-        id = setInterval(frame, 10);
-        function frame() {
-            if (pos === 5) {
-                clearInterval(id);
-            } else {
-                pos--;
-                notification.style.width = pos + "%";
-            }
-        }
-
-        notification.style.transition = "transform 25s ease";
-        notification.style.float = "right";
-        notification.style.marginBottom = "calc(40 * 0.063em)";
-        text_notification.style.display = "none";
-        arrow_id.style.display = "none";
-        x = 1;
-    } else {
-
-        x = 0;
-
-        let id = null;
-        let pos = 5;
-        clearInterval(id);
-        id = setInterval(frame, 1);
-        function frame() {
-            if (pos === 100) {
-                clearInterval(id);
-                text_notification.style.display = "block";
-                arrow_id.style.display = "flex";
-            } else {
-                pos++;
-                notification.style.width = pos + "%";
-            }
-        }
-
-    }
-}*/
-
-
 var x;
 if(x === undefined){
     x = true;
@@ -63,8 +16,14 @@ if(x === undefined){
 var notification = document.getElementById("notification");
 var text_notification = document.getElementById("text_notification");
 var arrow_id = document.getElementById("arrow_id");
+var cancel = document.getElementById("cancel");
 
-var speedAndTimout = 2500;
+var speedAndTimoutNotification = 2500;
+var speedProgressBarAnimation = 1000;
+
+var nunmberofallCheckboxes;
+var nunmberofCheckedCheckboxes;
+var percentageOfCheckedCheckboxes;
 
 // Function to increase image size
 function resizeNotification() {
@@ -73,13 +32,13 @@ function resizeNotification() {
         arrow_id.style.display = "none";
         $("div.notification").animate({
             width: '-=95%'
-        }, speedAndTimout);
+        }, speedAndTimoutNotification);
         x = false;
     } else {
         $("div.notification").animate({
             width: '+=95%'
-        }, speedAndTimout);
-        setTimeout(myGreeting, speedAndTimout);
+        }, speedAndTimoutNotification);
+        setTimeout(myGreeting, speedAndTimoutNotification);
         function myGreeting() {
             text_notification.style.display = "block";
             arrow_id.style.display = "flex";
@@ -88,6 +47,38 @@ function resizeNotification() {
     }
 }
 
+function numberOfCheckedCheckboxes (){
+    // console.log(document.querySelectorAll('input[type="checkbox"]:checked').length);
+
+    nunmberofallCheckboxes = document.querySelectorAll('input[type="checkbox"]').length;
+    nunmberofCheckedCheckboxes = document.querySelectorAll('input[type="checkbox"]:checked').length;
+
+    percentageOfCheckedCheckboxes = (nunmberofCheckedCheckboxes / nunmberofallCheckboxes) *100;
+
+    console.log(percentageOfCheckedCheckboxes);
+
+    $("div.progress_in_percent").animate({
+        width: +percentageOfCheckedCheckboxes+'%'
+    }, speedProgressBarAnimation);
+
+    document.getElementById("progress_in_percent").innerHTML = percentageOfCheckedCheckboxes+'%';
+    if(percentageOfCheckedCheckboxes === 0){
+        document.getElementById("progress_in_percent").innerHTML = '';
+    }
+
+    if(percentageOfCheckedCheckboxes === 100){
+        cancel.style.color = 'white';
+        cancel.style.background = '#687D99';
+        cancel.style.opacity = '1.0';
+    } else {
+        cancel.style.color = '#4C5A69';
+        cancel.style.background = '#E1E5EB';
+        cancel.style.opacity = '0.4';
+    }
+
+
+}
+let name = "Nathan";
 
 
 
