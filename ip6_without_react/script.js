ae6b22e77e2053a95d273eed1cfde527321972fd
joycehanggi <joyce.haenggi@students.fhnw.ch>
2022-01-25 13:13:37 +0100

    added js for get value of radio

--- ip6_without_react/script.js
+++ ip6_without_react/script.js
@@ -150,13 +150,43 @@ for (i = 0; i < acc.length; i++) {
 }
 
 
-// get value of the checked radio
-function displayRadioValue() {
+// get value of the checked radio for Severity
+function displayRadioValueOfSeverity() {
     var ele = document.getElementsByName('severity');
+
 
     for(i = 0; i < ele.length; i++) {
         if(ele[i].checked)
-            document.getElementById("result").innerHTML
-                = ele[i].value;
+            document.getElementById("resultSeverity").innerHTML
+                = ele[i].value ;
+    }
+}
+
+// get value of the checked radio for Severity_two
+function displayRadioValueOfSeverityTwo() {
+    var ele = document.getElementsByName('severity_two');
+
+
+    for(i = 0; i < ele.length; i++) {
+        if(ele[i].checked)
+            document.getElementById("resultSeverity_two").innerHTML
+                = ele[i].value ;
+    }
+}
+
+
+// get value of the checked radio for risk_priority
+function displayRadioValueOfRiskPriority() {
+    var ele = document.getElementsByName('risk_priority');
+
+
+    for(i = 0; i < ele.length; i++) {
+        if(ele[i].checked)
+            document.getElementById("resultRiskPriority").innerHTML
+                = "/"+ ele[i].value;
+        document.getElementById("resultRiskPriority_two").innerHTML
+            = ele[i].value;
+        document.getElementById("resultRiskPriority_three").innerHTML
+            = "/"+ele[i].value;
     }
 }
