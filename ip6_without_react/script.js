e759035fdf9a28adf52dd7b3c1fb27fad527010d
Florian Daasch <florian.daasch@students.fhnw.ch>
2022-01-26 20:01:34 +0100

    Updated variables

--- ip6_without_react/script.js
+++ ip6_without_react/script.js
@@ -22,8 +22,14 @@ let fullNotification;
 let checkbox;
 let item;
 
+var step = 0;
+let step_arr = ['CT-Scan Orbita Rekon', 'CT-Daten Segmentierung Orbita', '3D Patientenanatomie', 'Implantatdesign Matching', 'Pre-Processing for SLM Titan Orbita',
+                'SLM_Titan', 'Wärmebehandlung SLM Titan', 'Deburring SLM Titan', 'Sand-Blasting SLM Titan', 'Machining SLM Titan Structures'];
+
 window.onload = function () {
     numberOfCheckedCheckboxes();
+    names();
+    hazardChecklist();
     fullNotification = localStorage.getItem("someVarKey");
 
     if (fullNotification === "false") {
@@ -32,6 +38,12 @@ window.onload = function () {
         text_notification.style.arrow_id = "none";
     }
     localStorage.getItem('dropdownWasOpen')
+}
+
+function names(){
+    document.getElementById("actualStep").innerHTML = step_arr[step];
+    document.getElementById("titleName").innerHTML = step_arr[step];
+    document.getElementById("titleProgressBar").innerHTML = "Fortschrittsübersicht - "+step_arr[step];
 }
 
 if (fullNotification === undefined) {
@@ -92,6 +104,8 @@ function decreaseNotification() {
     localStorage.setItem("someVarKey", fullNotification);
 }
 
+
+
 function numberOfCheckedCheckboxes() {
     nunmberofallCheckboxes = document.querySelectorAll('input[type="checkbox"]').length;
     nunmberofCheckedCheckboxes = document.querySelectorAll('input[type="checkbox"]:checked').length;
@@ -140,6 +154,36 @@ nextStep.onmouseout = function () {
 };
 
 
+
+
+function evaluationYes(x) {
+    var y = document.getElementsByClassName("pP"+x).nextElementSibling;
+
+    postProcessingBlock.style.display = 'block';
+    postProcessingBlock.style.height =  "48px";
+    panel.style.maxHeight = panel.scrollHeight + "px";
+    // console.log(y);
+}
+
+function evaluationNo(x){
+    postProcessingBlock.style.display = 'none';
+    postProcessingBlock.style.height =  "0";
+}
+
+function nextStepCounter(){
+    step++;
+
+    nunmberofallCheckboxes = document.querySelectorAll('input[type="checkbox"]').length;
+    let i = 0;
+    for (i = 0; i < nunmberofallCheckboxes; i++) {
+        document.getElementById("confirm" + i).checked = false;
+    }
+    // numberOfCheckedCheckboxes()
+
+    names();
+    hazardChecklist();
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     checkbox = document.querySelectorAll("input[type='checkbox']");
 
@@ -160,53 +204,27 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
-function evaluationYes(x) {
-    var y = document.getElementsByClassName("pP"+x).nextElementSibling;
 
-    postProcessingBlock.style.display = 'block';
-    postProcessingBlock.style.height =  "48px";
-    panel.style.maxHeight = panel.scrollHeight + "px";
-    // console.log(y);
+// Hazards-Checklist
+function hazardChecklist(){
+
 }
-
-function evaluationNo(x){
-    postProcessingBlock.style.display = 'none';
-    postProcessingBlock.style.height =  "0";
-}
-
 
 var acc = document.getElementsByClassName("accordion");
-var i;
+var a;
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
+for (a = 0; a < acc.length; a++) {
+
+    acc[a].addEventListener("click", function () {
         this.classList.toggle("active");
         var panel = this.nextElementSibling;
         if (panel.style.maxHeight) {
             panel.style.maxHeight = null;
             panel.style.borderBottom = "none"
-            localStorage.setItem('dropdownWasOpen', 'true');
+            // localStorage.setItem('dropdownWasOpen', 'true');
         } else {
             panel.style.maxHeight = panel.scrollHeight + "px";
             panel.style.borderBottom = "1px solid #E1E5EB"
         }
     });
 }
-
-/*var acc2 = document.getElementsByClassName("accordion2");
-var j;
-
-for (j = 0; j < acc.length; j++) {
-    acc2[j].addEventListener("click", function () {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-            panel.style.borderBottom = "none"
-            localStorage.setItem('dropdownWasOpen', 'true');
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-            panel.style.borderBottom = "1px solid #E1E5EB"
-        }
-    });
-}*/
