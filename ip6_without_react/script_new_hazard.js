6ddb97e91315c327e6bd42d9a6c09aa5cc98abe7
Florian Daasch <florian.daasch@students.fhnw.ch>
2022-01-27 13:51:23 +0100

    Deleted table

--- ip6_without_react/script_new_hazard.js
+++ ip6_without_react/script_new_hazard.js
@@ -4,7 +4,7 @@ function displayRadioValueOfSeverity() {
     var ele = document.getElementsByName('severity');
 
 
-    for(i = 0; i < ele.length; i++) {
+    for(let i = 0; i < ele.length; i++) {
         if(ele[i].checked)
             document.getElementById("resultSeverity").innerHTML
                 = ele[i].value ;
@@ -16,7 +16,7 @@ function displayRadioValueOfSeverityTwo() {
     var ele = document.getElementsByName('severity_two');
 
 
-    for(i = 0; i < ele.length; i++) {
+    for(let i = 0; i < ele.length; i++) {
         if(ele[i].checked)
             document.getElementById("resultSeverity_two").innerHTML
                 = ele[i].value ;
@@ -28,17 +28,17 @@ function displayRadioValueOfSeverityTwo() {
 function displayRadioValueOfRiskPriority() {
     var ele = document.getElementsByName('risk_priority');
 
-    for(i = 0; i < ele.length; i++) {
+    for(let i = 0; i < ele.length; i++) {
         if(ele[i].checked)
             document.getElementById("resultRiskPriority").innerHTML
                 = ele[i].value;
     }
-    for(i = 0; i < ele.length; i++) {
+    for(let i = 0; i < ele.length; i++) {
         if(ele[i].checked)
             document.getElementById("resultRiskPriority_two").innerHTML
                 = ele[i].value;
     }
-    for(i = 0; i < ele.length; i++) {
+    for(let i = 0; i < ele.length; i++) {
         if(ele[i].checked)
             document.getElementById("resultRiskPriority_three").innerHTML
                 =  ele[i].value;
