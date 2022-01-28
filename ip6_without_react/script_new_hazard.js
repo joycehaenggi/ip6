37bd6f1ec0e4d1d92fb1fb4c4b850b83e037c2cb
Florian Daasch <florian.daasch@students.fhnw.ch>
2022-01-28 13:55:27 +0100

    Refactor script for form

--- ip6_without_react/script_new_hazard.js
+++ ip6_without_react/script_new_hazard.js
@@ -1,87 +1,82 @@
 'use strict';
-// get value of the checked radio for Severity
-function displayRadioValueOfSeverity() {
-    var ele = document.getElementsByName('severity');
 
+// get value of the checked radio for ProbabilityOfOccurrence
+function displayRadioValueOfProbabilityOfOccurrence(ProbabilityOfOccurrenceRadioButtonGroup){
+    let ProbabilityOfOccurrenceValue;
 
-    for(let i = 0; i < ele.length; i++) {
-        if(ele[i].checked)
-            document.getElementById("resultSeverity").innerHTML
-                = ele[i].value ;
+    if(ProbabilityOfOccurrenceRadioButtonGroup === 1){
+        ProbabilityOfOccurrenceValue = document.querySelector('input[name="probabilityOfOccurrence"]:checked').value;
+        document.getElementById("resultProbabilityOfOccurrence").innerHTML = ProbabilityOfOccurrenceValue;
+    } else {
+        ProbabilityOfOccurrenceValue = document.querySelector('input[name="probabilityOfOccurrenceTwo"]:checked').value;
+        document.getElementById("resultProbabilityOfOccurrence_two").innerHTML = ProbabilityOfOccurrenceValue;
     }
 }
 
-// get value of the checked radio for Severity_two
-function displayRadioValueOfSeverityTwo() {
-    var ele = document.getElementsByName('severity_two');
-
-
-    for(let i = 0; i < ele.length; i++) {
-        if(ele[i].checked)
-            document.getElementById("resultSeverity_two").innerHTML
-                = ele[i].value ;
-    }
-}
-
-
-// get value of the checked radio for risk_priority
+// get value of the checked radio for severity
 function displayRadioValueOfRiskPriority() {
-    var ele = document.getElementsByName('risk_priority');
+    let severityValue = document.querySelector('input[name="severity"]:checked').value;
 
-    for(let i = 0; i < ele.length; i++) {
-        if(ele[i].checked)
-            document.getElementById("resultRiskPriority").innerHTML
-                = ele[i].value;
-    }
-    for(let i = 0; i < ele.length; i++) {
-        if(ele[i].checked)
-            document.getElementById("resultRiskPriority_two").innerHTML
-                = ele[i].value;
-    }
-    for(let i = 0; i < ele.length; i++) {
-        if(ele[i].checked)
-            document.getElementById("resultRiskPriority_three").innerHTML
-                =  ele[i].value;
-    }
+    document.getElementById("resultSeverity").innerHTML = severityValue;
+    document.getElementById("resultSeverity_two").innerHTML = severityValue;
+    document.getElementById("resultSeverity_three").innerHTML = severityValue;
 }
-//
-// function addNewDamage(){
-//     var new_input="<input class='testtest' type='text'>";
-//     $('#new_damage').append(new_input);
-//
-// }
-//
-// function addNewHazard(){
-//     var new_div="<div class='testtesttest'></div>";
-//     var new_input="<input class='testtest'  type='text'>";
-//     $('#test').append(new_div);
-//     $('#new_hazard').append(new_input);
-//
-//     $('#mouseenter .testtesttest').css('margin-bottom', '220px');
-//
-// }
-//
-// function moveDiv(getDiv, getValue)
-// {
-//     var curMargin = parseInt(document.getElementById(getDiv).style.marginLeft);
-//     document.getElementById(getDiv).style.marginLeft = curMargin + getValue + "px";
-// }
+
+function addNewTextfield(TopicofnewTextfield){
+    var objTo = document.getElementById(TopicofnewTextfield);
+    var divAdd = document.createElement("div");
+    divAdd.innerHTML = '<input class="input_customized add_new_input" type="text">';
+    objTo.appendChild(divAdd)
+}
 
 
+
+//Long Variant for new TextFields in separated methods
 function addNewDamage() {
     var objTo = document.getElementById('add_new_Damage')
     var divAdd = document.createElement("div");
-    divAdd.innerHTML = '<input class="input_customized add_new_input" type="text" name="fname">';
+    divAdd.innerHTML = '<input class="input_customized add_new_input" type="text">';
     objTo.appendChild(divAdd)
-    // document.getElementById('form').style.height = "37rem";
-
 }
-
 function addNewMeasures() {
-
     var objTo2 = document.getElementById('add_new_measures')
     var divAdd2 = document.createElement("div");
-    divAdd2.innerHTML = '<input class="input_customized add_new_input" type="text" name="fname">';
+    divAdd2.innerHTML = '<input class="input_customized add_new_input" type="text">';
     objTo2.appendChild(divAdd2)
-    // document.getElementById('form').style.height = "37rem";
+}
+
+
+//Alternative Versions
+// get value of the checked radio for ProbabilityOfOccurrence
+function displayRadioValueOfProbabilityOfOccurrenceAlternative(x) {
+    var ele
+
+    if(x === 1){
+        ele = document.getElementsByName('severity');
+    } else {
+        ele = document.getElementsByName('severity_two');
+    }
+
+    for (let i = 0; i < ele.length; i++) {
+        if (ele[i].checked)
+            if(x === 1){
+                document.getElementById("resultSeverity").innerHTML = ele[i].value;
+            } else {
+                document.getElementById("resultSeverity_two").innerHTML = ele[i].value;
+            }
+    }
+}
+
+// get value of the checked radio for severity
+function displayRadioValueOfRiskPriorityAlternative() {
+    var ele = document.getElementsByName('risk_priority');
+
+    for (let i = 0; i < ele.length; i++) {
+        if (ele[i].checked) {
+            document.getElementById("resultSeverity").innerHTML = ele[i].value;
+            document.getElementById("resultSeverity_two").innerHTML = ele[i].value;
+            document.getElementById("resultSeverity_three").innerHTML = ele[i].value;
+            break;
+        }
+    }
 }
