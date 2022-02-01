4538f7003dd435c280ec9f6a47810f05c0b7abe6
joycehanggi <joyce.haenggi@students.fhnw.ch>
2022-02-01 11:02:45 +0100

    add vue.js framework

---
+++ ip6_with_vue/src/components/Navigation.vue
@@ -0,0 +1,300 @@
+svg<template>
+  <div class="wrapper">
+    <div class="manufacturing-step-navigation">
+      <div class="navigation-tree">
+        <svg class="home" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <g clip-path="url(#clip0_1387_56763)">
+            <path d="M14.8861 7.00049L12.1185 4.63702C12.1217 4.62085 12.1249 4.60469 12.1282 4.58529V1.49435C12.1249 1.31005 11.9794 1.16133 11.7952 1.15486H10.2497C10.0719 1.15486 9.92637 1.31329 9.92637 1.49435V2.77469L7.71163 0.886503C7.592 0.783041 7.41417 0.783041 7.29131 0.886503L0.116845 6.99726C-0.0221828 7.11042 -0.0383488 7.31411 0.0748131 7.45314C0.187975 7.59217 0.391666 7.60833 0.530694 7.49517C0.533927 7.49194 0.533927 7.49194 0.53716 7.4887L7.50147 1.55901L9.99426 3.68322L11.4848 4.95064L14.469 7.49194C14.6048 7.60833 14.8085 7.59216 14.9249 7.45637C15.0381 7.32058 15.0219 7.11688 14.8861 7.00049ZM11.4783 4.09061L10.573 3.32757V1.80473H11.4783V4.09061Z"
+                  fill="black"/>
+            <path d="M13.4601 7.65359L12.5516 6.87762L7.71149 2.74236C7.59186 2.6389 7.41403 2.6389 7.29117 2.74236L2.45107 6.87762L1.54255 7.65359C1.40675 7.76998 1.39059 7.97367 1.50698 8.10947C1.62014 8.24526 1.8206 8.26143 1.95316 8.14827L1.9564 8.14503L2.36054 7.79585V13.8581C2.36378 14.0392 2.51251 14.1879 2.69356 14.1911H12.3059C12.4869 14.1879 12.6356 14.0424 12.6389 13.8581V7.79585L13.043 8.14503C13.1012 8.19353 13.1724 8.22263 13.25 8.22263C13.3437 8.22263 13.4342 8.1806 13.4957 8.10947C13.6153 7.97044 13.5959 7.76675 13.4601 7.65359ZM8.79461 13.5412H6.20805V10.0494H8.79461V13.5412ZM9.44125 13.5412V9.73577C9.44125 9.55794 9.30869 9.40275 9.13086 9.40275H5.8718C5.69397 9.40275 5.56141 9.55794 5.56141 9.73577V13.5412H3.00718V7.24297L7.50133 3.41487L11.9955 7.24297V13.5412H9.44125Z"
+                  fill="black"/>
+          </g>
+          <defs>
+            <clipPath id="clip0_1387_56763">
+              <rect width="15" height="15" fill="white"/>
+            </clipPath>
+          </defs>
+        </svg>
+        <div class="next-arrow next-arrow-first-child">
+          <svg width="6" height="6" viewBox="0 0 6 6" fill="none"
+               xmlns="http://www.w3.org/2000/svg">
+            <g clip-path="url(#clip0_1387_56747)">
+              <path d="M4.55193 2.83534L1.77676 0.0678424C1.68583 -0.0227783 1.53862 -0.022626 1.44785 0.0683112C1.35715 0.159237 1.35738 0.306529 1.44832 0.39722L4.05831 3L1.44822 5.60277C1.3573 5.69348 1.35706 5.84068 1.44775 5.93161C1.49326 5.9772 1.55287 5.99999 1.61248 5.99999C1.67195 5.99999 1.73132 5.97735 1.77675 5.93208L4.55193 3.16465C4.59573 3.12108 4.6203 3.06178 4.6203 3C4.6203 2.93822 4.59566 2.879 4.55193 2.83534Z"
+                    fill="black"/>
+            </g>
+            <defs>
+              <clipPath id="clip0_1387_567471">
+                <rect width="6" height="6" fill="white"/>
+              </clipPath>
+            </defs>
+          </svg>
+        </div>
+        <p class="navigation-steps">Neuer Auftrag erstellen</p>
+        <div class="next-arrow">
+          <svg width="6" height="6" viewBox="0 0 6 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clip-path="url(#clip0_1387_56747)">
+              <path d="M4.55193 2.83534L1.77676 0.0678424C1.68583 -0.0227783 1.53862 -0.022626 1.44785 0.0683112C1.35715 0.159237 1.35738 0.306529 1.44832 0.39722L4.05831 3L1.44822 5.60277C1.3573 5.69348 1.35706 5.84068 1.44775 5.93161C1.49326 5.9772 1.55287 5.99999 1.61248 5.99999C1.67195 5.99999 1.73132 5.97735 1.77675 5.93208L4.55193 3.16465C4.59573 3.12108 4.6203 3.06178 4.6203 3C4.6203 2.93822 4.59566 2.879 4.55193 2.83534Z"
+                    fill="black"/>
+            </g>
+            <defs>
+              <clipPath id="clip0_1387_567472">
+                <rect width="6" height="6" fill="white"/>
+              </clipPath>
+            </defs>
+          </svg>
+        </div>
+        <p class="navigation-steps">Herstellungsprozess</p>
+        <div class="next-arrow">
+          <svg width="6" height="6" viewBox="0 0 6 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clip-path="url(#clip0_1387_56747)">
+              <path d="M4.55193 2.83534L1.77676 0.0678424C1.68583 -0.0227783 1.53862 -0.022626 1.44785 0.0683112C1.35715 0.159237 1.35738 0.306529 1.44832 0.39722L4.05831 3L1.44822 5.60277C1.3573 5.69348 1.35706 5.84068 1.44775 5.93161C1.49326 5.9772 1.55287 5.99999 1.61248 5.99999C1.67195 5.99999 1.73132 5.97735 1.77675 5.93208L4.55193 3.16465C4.59573 3.12108 4.6203 3.06178 4.6203 3C4.6203 2.93822 4.59566 2.879 4.55193 2.83534Z"
+                    fill="black"/>
+            </g>
+            <defs>
+              <clipPath id="clip0_1387_567473">
+                <rect width="6" height="6" fill="white"/>
+              </clipPath>
+            </defs>
+          </svg>
+        </div>
+        <p class="navigation-steps nameStep" id="actualStep"></p>
+      </div>
+      <div class="searchbar">
+        <label class="searchfield-label">
+          <input type="text" class="searchfield">
+        </label>
+      </div>
+    </div>
+
+    <div class="manufacturing-step-information">
+      <div class="menu-title-box">
+        <p class="manu-title" id="titleName"></p>
+      </div>
+      <div class="manufacturing-legend">
+        <div class="priority-title">
+          <p class="manu-legend-tittle">Risikopriorität:</p>
+        </div>
+        <div class="low">
+          <span class="legend-dot green"></span>
+          <p class="manu-legend">gering</p>
+        </div>
+        <div class="middle">
+          <span class="legend-dot yellow"></span>
+          <p class="manu-legend">mittel</p>
+        </div>
+        <div class="high">
+          <span class="legend-dot red"></span>
+          <p class="manu-legend">hoch</p>
+        </div>
+        <div class="manufacturing-legend-elements">
+        </div>
+      </div>
+    </div>
+
+    <div class="rectangle">
+      <div class="notification" onclick="decreaseNotification()" id="notification">
+        <div class="first_part">
+          <svg class="info_icon" id="info_icon" width="6" height="12" viewBox="0 0 6 12" fill="none"
+               xmlns="http://www.w3.org/2000/svg">
+            <path d="M5.14332 10.705L4.97565 11.3904C4.47264 11.5889 4.07084 11.7401 3.77164 11.8439C3.47211 11.9482 3.12408 12 2.72755 12C2.11861 12 1.64496 11.8508 1.30715 11.5545C0.969339 11.2571 0.80038 10.8801 0.80038 10.4228C0.80038 10.2458 0.81264 10.0637 0.83813 9.87816C0.863834 9.69243 0.904703 9.48324 0.960628 9.24943L1.58925 7.02446C1.64518 6.8114 1.69271 6.60953 1.73079 6.41863C1.7694 6.22892 1.788 6.05437 1.788 5.89724C1.788 5.61309 1.72917 5.41434 1.61205 5.3026C1.49493 5.19118 1.27144 5.13461 0.939656 5.13461C0.777149 5.13461 0.610126 5.16063 0.439876 5.21097C0.268873 5.26151 0.122713 5.31034 0 5.35573L0.168099 4.66978C0.579904 4.50211 0.973534 4.35853 1.35006 4.23937C1.72659 4.11988 2.08236 4.06008 2.41889 4.06008C3.02363 4.06008 3.49029 4.20624 3.81799 4.49856C4.14569 4.79109 4.30949 5.17031 4.30949 5.63761C4.30949 5.7343 4.29874 5.90466 4.27561 6.14815C4.25303 6.39218 4.21098 6.61577 4.14967 6.81915L3.52374 9.0352C3.47244 9.21319 3.4263 9.41667 3.38618 9.64564C3.34478 9.87311 3.32499 10.0469 3.32499 10.1635C3.32499 10.4577 3.39059 10.6586 3.52212 10.7654C3.65462 10.8722 3.88284 10.9254 4.207 10.9254C4.35918 10.9254 4.53233 10.8984 4.72474 10.8454C4.9166 10.7923 5.05653 10.7458 5.14332 10.705ZM5.30206 1.40136C5.30206 1.78746 5.15655 2.11721 4.86412 2.38834C4.57245 2.66044 4.22098 2.7966 3.80982 2.7966C3.39737 2.7966 3.04504 2.66044 2.75003 2.38834C2.45556 2.1171 2.308 1.78746 2.308 1.40136C2.308 1.01602 2.45556 0.685733 2.75003 0.41116C3.0445 0.137017 3.39747 0 3.80982 0C4.22087 0 4.57245 0.13734 4.86412 0.41116C5.15676 0.685733 5.30206 1.01612 5.30206 1.40136Z"
+                  fill="white"/>
+          </svg>
+          <p id="text_notification">Wählen Sie alle nicht zutreffenden Gefährdungen aus und bearbeiten Sie die
+            zutreffenden Gefährdungen</p>
+        </div>
+        <div class="arrow" id="arrow_id">
+          <svg class="arrow_right" width="15" height="15" viewBox="0 0 15 15" fill="none"
+               xmlns="http://www.w3.org/2000/svg">
+            <path d="M1.26063 8.12621H12.2396L10.1828 10.1831C9.59193 10.7739 10.4782 11.6601 11.069 11.0693L12.9423 9.19312L14.1928 7.94065C14.4351 7.69685 14.4351 7.3031 14.1928 7.0593L11.069 3.93187C10.9501 3.80974 10.7865 3.74122 10.6161 3.74264C10.0535 3.74271 9.77737 4.42799 10.1828 4.81813L12.2445 6.87504H1.22829C0.361791 6.91803 0.426489 8.16934 1.26063 8.12621Z"
+                  fill="white"/>
+          </svg>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navigation"
+}
+let notification = document.getElementById("notification");
+let text_notification = document.getElementById("text_notification");
+let fullNotification;
+
+var step = 0;
+let step_arr = ['CT-Scan Orbita Rekon', 'CT-Daten Segmentierung Orbita', '3D Patientenanatomie', 'Implantatdesign Matching', 'Pre-Processing for SLM Titan Orbita',
+  'SLM Titan', 'Wärmebehandlung SLM Titan', 'Deburring SLM Titan', 'Sand-Blasting SLM Titan', 'Machining SLM Titan Structures'];
+
+window.onload = function () {
+  names();
+  fullNotification = localStorage.getItem("someVarKey");
+
+  if (fullNotification === "false") {
+    notification.style.width = "5%";
+    text_notification.style.display = "none";
+    text_notification.style.arrow_id = "none";
+  }
+  localStorage.getItem('dropdownWasOpen')
+}
+
+function names() {
+  document.getElementById("actualStep").innerHTML = step_arr[step];
+  document.getElementById("titleName").innerHTML = step_arr[step];
+}
+
+// TODO Animate Notification
+
+</script>
+
+<style scoped>
+/*---- Wrapper ----*/
+.wrapper {
+  margin-top: calc(var(--marginOne) * 0.063rem);
+  padding-left: var(--paddingLeftRight);
+  padding-right: var(--paddingLeftRight);
+  margin-bottom: calc(var(--marginOne) * 0.063rem);
+}
+
+
+/*---- Top-overview ----*/
+.manufacturing-step-navigation {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  font-family: 'OpenSans-Bold', sans-serif;
+  font-size: calc(11 * 0.063rem);
+  margin-top: calc(20 * 0.063rem);
+}
+
+.navigation-tree {
+  display: flex;
+  flex-direction: row;
+}
+
+.next-arrow {
+  margin-left: calc(15 * 0.063rem);
+  margin-right: calc(5 * 0.063rem);
+}
+
+.next-arrow-first-child {
+  margin-left: calc(12 * 0.063rem);
+}
+
+input.searchfield {
+  box-sizing: border-box;
+  border-radius: 50px;
+  border: 1px solid var(--notification-Blue);
+  height: 21.8px;
+  width: 200px;
+  padding-left: 25px;
+
+}
+
+.searchfield-label {
+  position: relative;
+}
+
+.searchfield-label:before {
+  content: "";
+  position: absolute;
+  left: 5px;
+  top: 2px;
+  bottom: 0;
+  width: 20px;
+  background: url("../assets/img/search.svg") center / contain no-repeat;
+}
+
+.manufacturing-step-information {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-family: 'OpenSans-Regular', sans-serif;
+  height: 21px;
+  margin-top: 16px;
+  margin-bottom: 20px;
+}
+
+.manu-title {
+  font-size: calc(14 * 0.063rem);
+}
+
+.manufacturing-legend {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 21px;
+  font-size: calc(11 * 0.063rem);
+}
+
+.manufacturing-legend-elements {
+  display: flex;
+  font-size: calc(11 * 0.063rem);
+  margin-top: calc(1.4 * 0.063rem);
+}
+
+.low, .middle, .high {
+  display: flex;
+  width: 60px;
+  justify-content: space-between;
+  align-items: center;
+  font-size: calc(11 * 0.063rem);
+  margin-left: 9px;
+}
+
+.legend-dot {
+  height: 21px;
+  width: 21px;
+  border-radius: 50%;
+  display: inline-block;
+}
+
+/*---- Notification-panel ----*/
+.rectangle {
+  display: block;
+  height: 30px;
+}
+
+.notification {
+  height: 30px;
+  width: 100%;
+  background-color: var(--notification-Blue);
+  color: white;
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  font-size: calc(calc(var(--mainFontSize)) * 0.063rem);
+  border-radius: 5px;
+  cursor: pointer;
+  float: right;
+  margin-bottom: calc(var(--marginOne) * 0.063rem);
+}
+
+.notification:active p, .notification:active .arrow_right {
+  display: none;
+}
+
+.first_part {
+  display: flex;
+}
+
+.arrow {
+  display: flex;
+  align-items: center;
+}
+
+.info_icon, .arrow_right {
+  margin: 0 25px;
+}
+
+.dot {
+  height: 22px;
+  width: 22px;
+  border-radius: 50%;
+  display: inline-block;
+}
+
+
+
+</style>
