09423b50b18fcc33e1ff7c6fa4f49bdfd7d29f8f
joycehanggi <joyce.haenggi@students.fhnw.ch>
2022-02-01 15:08:43 +0100

    add unversioned files

---
+++ ip6_with_vue/src/views/NeueGefaehrdungErstellen.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <Header/>
+    <NavigationNeueGefaehrdung/>
+    <NeueGefaehrdungErstellen/>
+    <ButtonsNeueGefaehrdung/>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from '@/components/Header.vue'
+import NavigationNeueGefaehrdung from '@/components/NavigationNeueGefaehrdung.vue'
+import NeueGefaehrdungErstellen from "@/components/NeueGefaehrdungErstellen";
+import ButtonsNeueGefaehrdung from "@/components/ButtonsNeueGefaehrdung";
+
+
+export default {
+  name: 'Home',
+  components: {
+    Header,
+    NavigationNeueGefaehrdung,
+    NeueGefaehrdungErstellen,
+    ButtonsNeueGefaehrdung
+  }
+}
+</script>
